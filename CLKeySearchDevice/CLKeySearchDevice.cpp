1f9f6140f3110327c4a35134d95b09f0d52acd2d
RB61 <44677043+RB61@users.noreply.github.com>
2020-03-08 12:38:01 +0100

    Update CLKeySearchDevice.cpp

--- CLKeySearchDevice/CLKeySearchDevice.cpp
+++ CLKeySearchDevice/CLKeySearchDevice.cpp
@@ -575,7 +575,7 @@ void CLKeySearchDevice::generateStartingPoints()
 
 secp256k1::uint256 CLKeySearchDevice::getNextKey()
 {
-    uint64_t totalPoints = (uint64_t)_points * _threads * _blocks;
+    uint64_t totalPoints = (uint64_t)_points ;
 
     return _start + secp256k1::uint256(totalPoints) * _iterations * _stride;
 }
